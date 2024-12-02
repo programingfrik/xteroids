8e40fcc4cfbfd2ac9003dbea9fe8b29ecbaad3c8
Pablo Mercader Alcántara <programingfrik@gmail.com>
2024-12-01 21:16:53 -0400

    Limpiando el código, quitando mensajes de depuración.

--- src/movni.py
+++ src/movni.py
@@ -12,6 +12,91 @@ Y = 1
 MIN = 0
 MAX = 1
 
+def estaentre(v, ra, rb):
+    """Dice si v está entre el rango formado por ra y rb, no importa el orden."""
+    return (((ra < rb) and (v >= ra) and (v <= rb))
+            or ((rb < ra) and (v >= rb) and (v <= ra)))
+
+def puntoestaentre(ps, pi, pf):
+    """Asumiendo que los tres puntos ps, pi y pf estan sobre la misma
+    recta verifica si ps está entre pi y pf."""
+    global X, Y
+    return (estaentre(ps[X], pi[X], pf[X])
+            and estaentre(ps[Y], pi[Y], pf[Y]))
+
+def sesolapadim(ai, af, bi, bf):
+    """Dice si los rangos de ai a af y de bi a bf se tocan en una dimensión."""
+    return (estaentre(ai, bi, bf)
+        or estaentre(af, bi, bf)
+        or ((ai <= bi) and (af >= bf)))
+
+def sesolapan(ovniA, ovniB):
+    """Dice si los limites de los ovnis en cuestión se solapan."""
+    global X, Y, MIN, MAX
+    a = ovniA.lim
+    b = ovniB.lim
+    return (sesolapadim(a[MIN][X], a[MAX][X], b[MIN][X], b[MAX][X])
+        and sesolapadim(a[MIN][Y], a[MAX][Y], b[MIN][Y], b[MAX][Y]))
+
+def pendiente(a, b):
+    """Calcula la pendiente de la recta por la que pasan los puntos a y
+    b."""
+    global X, Y
+    if (b[X] - a[X]) == 0:
+        return None
+    return (b[Y] - a[Y]) / (b[X] - a[X])
+
+def secruzan(a1, a2, b1, b2):
+    """Verifica si la linea A compuesta por los puntos a1 y a2 se cruza
+    con la linea B compuesta por los puntos b1 y b2. Si se cruzan
+    aunque sea en un punto lejano retorna el punto de intersección
+    sino retorna None, o sea que son paralelas."""
+    global X, Y
+    ma = pendiente(a1, a2)
+    mb = pendiente(b1, b2)
+    if (ma == mb):
+        return None
+    elif (ma == None):
+        crucex = a1[X]
+        crucey = mb * (crucex - b1[X]) + b1[Y]
+    elif (mb == None):
+        crucex = b1[X]
+        crucey = ma * (crucex - a1[X]) + a1[Y]
+    else:
+        crucex = ((-mb * b1[X]) + b1[Y] - a1[Y] + (ma * a1[X])) / (ma - mb)
+        crucey = ma * (crucex - a1[X]) + a1[Y]
+    return (crucex, crucey)
+
+def distancia(a, b):
+    """Calcula la distancia entre dos puntos a y b"""
+    global X, Y
+    return math.sqrt((b[X] - a[X]) ** 2 + (b[Y] - a[Y]) ** 2)
+
+def baleado(ovni, puntob):
+    """Verifica si bala toca realmente alguna linea de ovni."""
+    for linea in ovni.lineas:
+        p1 = ovni.dibpuntos[linea[0]]
+        p2 = ovni.dibpuntos[linea[1]]
+        cruce = secruzan(p1, p2, puntob, ovni.loc)
+        if ((not cruce) or (not puntoestaentre(cruce, p1, p2))):
+            continue
+        if (distancia(cruce, ovni.loc)
+            > distancia(puntob, ovni.loc)):
+            return cruce
+    return None
+
+def colisionado(ovniA, ovniB):
+    """Verifica si los ovnis realmente se tocan entre ellos."""
+    valor = distancia(ovniA.loc, ovniB.loc)
+    if valor > (ovniA.radiomayor + ovniB.radiomayor):
+        return None
+    for punto in ovniA.dibpuntos:
+        if (distancia(punto, ovniB.loc) < ovniB.radiomayor):
+            choque = baleado(ovniB, punto)
+            if choque:
+                return choque
+    return None
+
 class Espacio:
     """Esta clase representa el espacio, cada instancia de esta clase
     sostiene unos objetos y los actualiza o los mueve y hace que
@@ -43,98 +128,6 @@ class Espacio:
         # se quita el objeto de la lista de objetos
         self.lovnis.remove(ovni)
 
-    def estaentre(self, v, ra, rb):
-        """Dice si v está entre el rango formado por ra y rb, no importa el orden."""
-        return (((ra < rb) and (v >= ra) and (v <= rb))
-                or ((rb < ra) and (v >= rb) and (v <= ra)))
-
-    def puntoestaentre(self, ps, pi, pf):
-        """Asumiendo que los tres puntos ps, pi y pf estan sobre la misma
-        recta verifica si ps está entre pi y pf."""
-        global X, Y
-        return (self.estaentre(ps[X], pi[X], pf[X])
-                and self.estaentre(ps[Y], pi[Y], pf[Y]))
-
-    def sesolapadim(self, ai, af, bi, bf):
-        """Dice si los rangos de ai a af y de bi a bf se tocan en una dimensión."""
-        return (self.estaentre(ai, bi, bf)
-            or self.estaentre(af, bi, bf)
-            or ((ai <= bi) and (af >= bf)))
-
-    def sesolapan(self, ovniA, ovniB):
-        """Dice si los limites de los ovnis en cuestión se solapan."""
-        global X, Y, MIN, MAX
-        a = ovniA.lim
-        b = ovniB.lim
-        return (self.sesolapadim(a[MIN][X], a[MAX][X], b[MIN][X], b[MAX][X])
-            and self.sesolapadim(a[MIN][Y], a[MAX][Y], b[MIN][Y], b[MAX][Y]))
-
-    def pendiente(self, a, b):
-        """Calcula la pendiente de la recta por la que pasan los puntos a y
-        b."""
-        global X, Y
-        if (b[X] - a[X]) == 0:
-            # print(f"Pendiente infinita {a} {b}")
-            return None
-        return (b[Y] - a[Y]) / (b[X] - a[X])
-
-    def secruzan(self, a1, a2, b1, b2):
-        """Verifica si la linea A compuesta por los puntos a1 y a2 se cruza
-        con la linea B compuesta por los puntos b1 y b2. Si se cruzan
-        aunque sea en un punto lejano retorna el punto de intersección
-        sino retorna None, o sea que son paralelas."""
-        global X, Y
-        ma = self.pendiente(a1, a2)
-        mb = self.pendiente(b1, b2)
-        if (ma == mb):
-            return None
-        elif (ma == None):
-            # print(f"Se produjo una pendiente infinita {ma}, por {a1} {a2}, sin embargo {b1} {b2} si tienen pendiente {mb}")
-            crucex = a1[X]
-            crucey = mb * (crucex - b1[X]) + b1[Y]
-        elif (mb == None):
-            # print(f"Se produjo una pendiente infinita {mb}, por {b1} {b2}, sin embargo {a1} {a2} si tienen pendiente {ma}")
-            crucex = b1[X]
-            crucey = ma * (crucex - a1[X]) + a1[Y]
-        else:
-            crucex = ((-mb * b1[X]) + b1[Y] - a1[Y] + (ma * a1[X])) / (ma - mb)
-            crucey = ma * (crucex - a1[X]) + a1[Y]
-        return (crucex, crucey)
-
-    def distancia(self, a, b):
-        """Calcula la distancia entre dos puntos a y b"""
-        global X, Y
-        return math.sqrt((b[X] - a[X]) ** 2 + (b[Y] - a[Y]) ** 2)
-
-    def baleado(self, ovni, puntob):
-        """Verifica si bala toca realmente alguna linea de ovni."""
-        for linea in ovni.lineas:
-            p1 = ovni.dibpuntos[linea[0]]
-            p2 = ovni.dibpuntos[linea[1]]
-            cruce = self.secruzan(p1, p2, puntob, ovni.loc)
-            if ((not cruce) or (not self.puntoestaentre(cruce, p1, p2))):
-                continue
-            if (self.distancia(cruce, ovni.loc)
-                  > self.distancia(puntob, ovni.loc)):
-                return cruce
-        return None
-
-    def colisionado(self, ovniA, ovniB):
-        """Verifica si los ovnis realmente se tocan entre ellos."""
-        # print(f"Posible colisión entre 2 ovnis {ovniA} y {ovniB}")
-        valor = self.distancia(ovniA.loc, ovniB.loc)
-        # print(f"Revisando la distancia {valor} {ovniA.radiomayor} {ovniB.radiomayor} {ovniA.radiomayor + ovniB.radiomayor}")
-        if valor > (ovniA.radiomayor + ovniB.radiomayor):
-            return None
-        for punto in ovniA.dibpuntos:
-            if (self.distancia(punto, ovniB.loc) < ovniB.radiomayor):
-                choque = self.baleado(ovniB, punto)
-                if choque:
-                    # print(f"Si hubo choque {choque}")
-                    return choque
-        # print(f"No no hubo choque")
-        return None
-
     def colisionares(self, lovni, ovni):
         """Verifica las colisiones de todos los ovnis y avisa a los ovnis
         involucrados en esas colisiones."""
@@ -155,28 +148,25 @@ class Espacio:
                 or (type(covni) not in [Bala, Nave, Meteoro])
                 or (isinstance(covni, Bala)
                     and isinstance(ovni, Bala))
-                or (not self.sesolapan(covni, ovni))):
+                or (not sesolapan(covni, ovni))):
                 continue
-            # print(f"Dos objetos se solapan {covni} y {ovni}")
-            # print(f"Verificando tipos {type(covni) in [Nave, Meteoro]} {type(ovni) in [Nave, Meteoro]}")
 
             # Está tocandose el cuadro límite, verifica si realmente
             # toca la figura
             if ((type(ovni) in [Nave, Meteoro])
                  and isinstance(covni, Bala)):
-                puntochoque = self.baleado(ovni, covni.punta())
+                puntochoque = baleado(ovni, covni.punta())
             elif ((type(covni) in [Nave, Meteoro])
                   and isinstance(ovni, Bala)):
-                puntochoque = self.baleado(covni, ovni.punta())
+                puntochoque = baleado(covni, ovni.punta())
             elif ((type(ovni) in [Nave, Meteoro])
                   and (type(covni) in [Nave, Meteoro])):
-                puntochoque = self.colisionado(ovni, covni)
+                puntochoque = colisionado(ovni, covni)
 
             if not puntochoque:
                 continue
 
             # Si se tocan
-            # print(f"Colisión detectada!")
             ovni.colision(covni, puntochoque)
             covni.colision(ovni, puntochoque)
 
@@ -194,7 +184,6 @@ class Espacio:
 
         # para todos los objetos en la pantalla
         tlovnis = self.lovnis.copy()
-        # print(f"Los ovnis que vamos a verificar {tlovnis}")
         for ovni in tlovnis:
             ovni.golpe()
             self.colisionares(tlovnis, ovni)
@@ -213,7 +202,6 @@ class Ovni:
     def __init__(self, loc, ang, vectord, colorApl):
         # inicializando variables
         self.ponerloccien(loc)
-        # print("creando un ovni nuevo loc:", loc, " self.loc:", self.loc)
         self.ang = ang
         self.vectord = vectord
         self.color = colorApl
@@ -225,7 +213,6 @@ class Ovni:
         # se inicialicen los puntos del dibujo
         self.lim = [(0, 0), (0, 0)]
         self.rotar(0)
-        # print("creando un ovni confirmacion loc:", loc, " self.loc:", self.loc)
         # De momento este ovni no es parte de ningún espacio
         self.miespacio = None
         # El cuadrado de los límites de este Ovni expresado en dos
@@ -245,8 +232,6 @@ class Ovni:
         global X, Y, MIN, MAX
 
         # aqui se manda a dibujar usando los puntos del dibujo
-        # print("dibujando: ", type(self))
-
         srfce = self.miespacio.pantalla
 
         if self.miespacio.depuracion:
@@ -284,8 +269,6 @@ class Ovni:
         """Rota un punto en el angulo cuyo seno y coseno se incluyen
         como parametros y retorna el punto resultante."""
         global X, Y
-
-        # print("seno %f y coseno %f y el punto (%i,%i)"%(sinAngul, cosAngul, punto[X], punto[Y]))
 
         # como el seno y el coseno son el mismo para todos los puntos
         # porque se trata del mismo angulo este se saca fuera de esta
@@ -424,7 +407,6 @@ class Omasa(Ovni):
         que el ovni en cuestión tome la acción que corresponda. ovnicol es el
         objeto con el que se produjo la colisión."""
         # Si hubo una colisión cambiale el color de depuración.
-        # print(f"{self} procesando colision")
         self.colordep = (0, 255, 0)
 
     def __repr__(self):
@@ -488,7 +470,6 @@ class Nave(Omasa):
         """La representación de esta Nave."""
         return f"<Nave loc={self.loc} ang={self.ang} lim={self.lim}>"
 
-
 class Meteoro(Omasa):
     def __init__(self, loc, ang, vectord, colorApl, masa, vel, vrot):
         Omasa.__init__(self, loc, ang, vectord, colorApl, masa)
@@ -514,11 +495,6 @@ class Meteoro(Omasa):
         self.lineas = []
         for cont in range((len(self.puntos) - 1), -1, -1):
             self.lineas.append((cont, cont - 1))
-
-#        for c in range(len(self.puntos)):
-#             print("self.puntos[%i] : %s " % (c, str(self.puntos[c])))
-#         for c in range(len(self.lineas)):
-#            print("self.lineas[%i] : %s " % (c, str(self.lineas[c])))
 
         self.rotar(0)
         self.acelerar(vel)
